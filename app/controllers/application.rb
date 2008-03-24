029c62132b075deeff54fe594140c42ee1c2f8b7
daisy <karenhawthorn@gmail.com>
2008-03-24 22:22:55 +0000

    washed mouth out

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -3,7 +3,7 @@
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
-ARSE LICKING BUM SUCK
+
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => 'd48d5948d8b38f4f89d8cfb2b92a97f7'
